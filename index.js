1d44b05d46d57827aa175d76c01bf48eef6839a2
LegolasZidane <laraibaa6@gmail.com>
2025-07-10 20:30:16 +0530

    Initial commit for family tracker app

---
+++ index.js
@@ -0,0 +1,113 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "password",
+  port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let currentUserId = 0;
+
+let users = [];
+
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries WHERE user_id = $1", [currentUserId]);
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+
+
+app.get("/", async (req, res) => {
+  const result = await db.query("SELECT id, name, color FROM users;");
+  users = result.rows;
+  const countries = await checkVisisted();
+  res.render("index.ejs", { 
+    countries: countries,
+    total: countries.length,
+    users: users,
+    color: users[currentUserId].color
+  });
+});
+
+
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE $1 || '%';",
+      [input.toLowerCase()]
+    );
+
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
+        [countryCode, currentUserId]
+      );
+      res.redirect("/");
+    } catch (err) {
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        total: countries.length,
+        countries: countries,
+        error: "Country already exists, try again.",
+        users: users,
+        color: users[currentUserId].color
+      });
+    }
+  } catch (err) {
+    const countries = await checkVisisted();
+      res.render("index.ejs", {
+        total: countries.length,
+        countries: countries,
+        error: "Did not match with saved countries, try again.",
+        users: users,
+        color: users[currentUserId].color
+      });
+  }
+});
+
+
+app.post("/user", async (req, res) => {
+
+  if( req.body.add )
+    res.render("new.ejs");
+  else {
+    currentUserId = req.body.user;
+    res.redirect("/");
+  }
+});
+
+app.post("/new", async (req, res) => {
+  //Hint: The RETURNING keyword can return the data that was inserted.
+  //https://www.postgresql.org/docs/current/dml-returning.html
+  const newUser = {
+    id: users.length,
+    name: req.body.name,
+    color: req.body.color
+  };
+  
+  await db.query("INSERT INTO users (id, name, color) VALUES ($1, $2, $3)", [newUser.id, newUser.name, newUser.color]);
+  users.push(newUser);
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
