ff141717b4d3098a26bd2cca2a891a15b04b497b
LegolasZidane <laraibaa6@gmail.com>
2025-07-10 22:12:41 +0530

    fix: correct listen address log

--- index.js
+++ index.js
@@ -109,5 +109,5 @@ app.post("/new", async (req, res) => {
 });
 
 app.listen(port, "0.0.0.0",() => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://0.0.0.0:${port}`);
 });
